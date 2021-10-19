eaf061fc37e3b0bbb3912e7b30cd85ee2e1c08d6
Robert Poręba <webberr14@gmail.com>
2021-10-19 20:57:47 +0200

    init

---
+++ isn2018-3-backend.Infrastructure/Context.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isn2018_3_backend.Infrastructure
+{
+    class Context
+    {
+    }
+}
